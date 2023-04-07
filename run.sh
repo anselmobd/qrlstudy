859d318c679fdfa2b45cf0d910201c972ef5fa1c
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2023-04-07 16:39:04 -0300

    + tipos "e" e "f"

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-./qgye.py a 30000 1000 -i1000
-./qgye.py b 30000 1000 -i1000
-./qgye.py c 30000 1000 -i1000
-./qgye.py d 30000 1000 -i1000
+# ./qgye.py a 30000 1000 -i1000
+# ./qgye.py b 30000 1000 -i1000
+# ./qgye.py c 30000 1000 -i1000
+# ./qgye.py d 30000 1000 -i1000
+./qgye.py e 30000 1000 -i1000
+./qgye.py f 30000 1000 -i1000
