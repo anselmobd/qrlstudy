630a8a0737a3d96bfed79af83253bfef08e8d83f
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2023-04-12 10:59:59 -0300

    run.sh preparado para rodar a analise completa

--- run.sh
+++ run.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-# ./qgye.py a 30000 1000 -i1000
-# ./qgye.py b 30000 1000 -i1000
-# ./qgye.py c 30000 1000 -i1000
-# ./qgye.py d 30000 1000 -i1000
+./qgye.py a 30000 1000 -i1000
+./qgye.py b 30000 1000 -i1000
+./qgye.py c 30000 1000 -i1000
+./qgye.py d 30000 1000 -i1000
 ./qgye.py e 30000 1000 -i1000
 ./qgye.py f 30000 1000 -i1000
+
+find ./qtb/qgye1/ -name "*.qtb" -exec ./qeval.py {} 100 100 -q \; | tee ./qtb/qgye1/qeval.txt
