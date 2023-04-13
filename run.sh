d37b277ae207bac9b87afccbf1d01551d715ac7d
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2023-04-12 21:25:23 -0300

    run.sh inserida opção 'g'

--- run.sh
+++ run.sh
@@ -6,5 +6,6 @@
 ./qgye.py d 30000 1000 -i1000
 ./qgye.py e 30000 1000 -i1000
 ./qgye.py f 30000 1000 -i1000
+./qgye.py g 30000 1000 -i1000
 
 find ./qtb/qgye1/ -name "*.qtb" -exec ./qeval.py {} 100 100 -q \; | tee ./qtb/qgye1/qeval.txt
